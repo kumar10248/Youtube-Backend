9af26a5796a8c76c2c2e5aa467f594fbb90a22ec
kumar10248 <kumardevashish000@gmail.com>
2024-11-28 22:12:32 +0530

    add cookie-parser and cors middleware; implement ApiResponse and ApiError classes; create asyncHandler utility

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({limit: "30mb"}));
+app.use(express.urlencoded({limit: "30mb", extended: true}));
+app.use(express.static("public"));
+app.use(bodyParser.json());
+
+export { app };
