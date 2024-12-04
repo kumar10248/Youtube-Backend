4ebdb22aa3e4778ae4aebda6728598b9f81ef143
kumar10248 <kumardevashish000@gmail.com>
2024-12-04 23:07:02 +0530

    fix: simplify CORS configuration and clean up cookie handling in user login response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -133,8 +133,7 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 
     return res.status(200)
-        .cookie("accessToken", accessToken, options)
-        .cookie("refreshToken", refreshToken, options)
+     
         .json(
             new ApiResponse(
                 200,
